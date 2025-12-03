d0b6abcdd39d478525416dd310c5cf56d39199da
sungbin <dusvlf5950@naver.com>
2025-12-03 16:48:17 +0900

    infra: spring boot 세팅

---
+++ apiwiki-backend/src/main/java/com/umc/apiwiki/ApiwikiBackendApplication.java
@@ -0,0 +1,13 @@
+package com.umc.apiwiki;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiwikiBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiwikiBackendApplication.class, args);
+	}
+
+}
---
+++ apiwiki-backend/src/test/java/com/umc/apiwiki/ApiwikiBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.umc.apiwiki;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiwikiBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
