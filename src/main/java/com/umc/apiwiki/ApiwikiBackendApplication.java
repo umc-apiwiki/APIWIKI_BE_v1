393ce2cc9eb352cb3cef0f858620de99dbdc3125
sungbin <dusvlf5950@naver.com>
2026-01-09 15:10:03 +0900

    fix: 글리치 에러 모니터링 연동 및 설정 추가

--- src/main/java/com/umc/apiwiki/ApiwikiBackendApplication.java
+++ src/main/java/com/umc/apiwiki/ApiwikiBackendApplication.java
@@ -4,14 +4,11 @@ import io.sentry.Sentry;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.beans.factory.annotation.Value;
 
 @SpringBootApplication
 public class ApiwikiBackendApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ApiwikiBackendApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(ApiwikiBackendApplication.class, args);
+    }
 }
