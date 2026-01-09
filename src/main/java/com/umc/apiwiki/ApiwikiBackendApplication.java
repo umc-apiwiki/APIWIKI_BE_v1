f5448d9b0d2386401805c8274413537d0bb3b947
sungbin <dusvlf5950@naver.com>
2026-01-09 14:31:35 +0900

    fix: 에러 트레킹 오류 수정 및 테스트

--- src/main/java/com/umc/apiwiki/ApiwikiBackendApplication.java
+++ src/main/java/com/umc/apiwiki/ApiwikiBackendApplication.java
@@ -1,7 +1,11 @@
 package com.umc.apiwiki;
 
+import io.sentry.Sentry;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.beans.factory.annotation.Value;
 
 @SpringBootApplication
 public class ApiwikiBackendApplication {
