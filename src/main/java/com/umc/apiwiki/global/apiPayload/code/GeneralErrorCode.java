8c62ce97e7161bdfeb93dd27019caaefd6a11ea9
Seohyun Kim <canna7610@gmail.com>
2026-01-16 16:43:06 +0900

    fix : 404 경로 없음 에러를 위한 에러 핸들러 수정

--- src/main/java/com/umc/apiwiki/global/apiPayload/code/GeneralErrorCode.java
+++ src/main/java/com/umc/apiwiki/global/apiPayload/code/GeneralErrorCode.java
@@ -19,7 +19,7 @@ public enum GeneralErrorCode implements BaseErrorCode{
             "금지된 요청입니다."),
     NOT_FOUND(HttpStatus.NOT_FOUND,
             "COMMON404",
-            "요청한 리소스를 찾을 수 없습니다."),
+            "요청한 리소스를 찾을 수 없습니다. 해당 경로는 존재하지 않습니다."),
     INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR,
             "COMMON500",
             "서버 에러, 관리자에게 문의 바랍니다."),
