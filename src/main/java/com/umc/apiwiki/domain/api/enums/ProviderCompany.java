01b95c24f50c18f21ce823903b5f795e0103f0fe
Seohyun Kim <canna7610@gmail.com>
2026-01-26 18:58:51 +0900

    feat: api를 위한 회사 enum 추가

--- src/main/java/com/umc/apiwiki/domain/api/enums/ProviderCompany.java
+++ src/main/java/com/umc/apiwiki/domain/api/enums/ProviderCompany.java
@@ -7,9 +7,55 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public enum ProviderCompany {
 
+    // [국내 기업]
     KAKAO("카카오"),
     NAVER("네이버"),
-    GOOGLE("구글"),
+
+    // [글로벌 빅테크]
+    GOOGLE("Google"),
+    MICROSOFT("Microsoft"),
+    AMAZON("Amazon"), // AWS 포함
+    META("Meta"),     // Facebook, Instagram
+    IBM("IBM"),
+    APPLE("Apple"),
+
+    // [SaaS & API 전문 기업]
+    SPOTIFY("Spotify"),
+    ATLASSIAN("Atlassian"), // Jira, Trello
+    OPEN_WEATHER("OpenWeather"),
+    TELEGRAM("Telegram"),
+    MIXPANEL("Mixpanel"),
+    STRIPE("Stripe"),
+    LINKEDIN("LinkedIn"),
+    DISCORD("Discord"),
+    ASANA("Asana"),
+    WOLFRAM("Wolfram"),
+    NOTION("Notion"),
+    HUBSPOT("HubSpot"),
+    PEXELS("Pexels"),
+    SLACK("Slack"),
+    OPEN_STREET_MAP("OpenStreetMap Foundation"),
+    PAYPAL("PayPal"),
+    REDDIT("Reddit"),
+    DROPBOX("Dropbox"),
+    DEEPL("DeepL"),
+    TWILIO("Twilio"),
+    NEWS_API("NewsAPI"),
+    OPEN_AI("OpenAI"),
+    MAILCHIMP("Mailchimp"),
+    SHOPIFY("Shopify"),
+    SQUARE("Square"),
+    AMPLITUDE("Amplitude"),
+    ZOOM("Zoom"),
+    AUTOMATTIC("Automattic"), // WooCommerce
+    UNSPLASH("Unsplash"),
+    SENDGRID("SendGrid"),
+    GIPHY("Giphy"),
+    GITHUB("GitHub"),
+    TWITTER("X(Twitter)"),
+    SALESFORCE("Salesforce"),
+
+    // [기타]
     OPEN_DATA("공공데이터포털"),
     ETC("기타");
 
