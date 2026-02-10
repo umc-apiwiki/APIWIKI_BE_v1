e9cda6e444b6937a9837fcb61ec2ddf4ffe7420e
SYDLK <kmj977284@gmail.com>
2026-02-10 19:55:40 +0900

    refactor[api]: 사용하지 않는 import 삭제

--- src/main/java/com/umc/apiwiki/domain/api/service/query/ApiDetailQueryService.java
+++ src/main/java/com/umc/apiwiki/domain/api/service/query/ApiDetailQueryService.java
@@ -12,9 +12,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.util.HashSet;
 import java.util.List;
-import java.util.Set;
 
 @Service
 @RequiredArgsConstructor
